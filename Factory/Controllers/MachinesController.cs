bab0cf9bcb81ce17f2b88b04d334bce64720c3ad
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-07 10:41:01 -0900

    add edit post

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -44,5 +44,12 @@ namespace Factory.Controllers
         var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
         return View(thisMachine);
       }
+      [HttpPost]
+      public ActionResult Edit(Machine machine)
+      {
+        _db.Entry(machine).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
     }
 }
