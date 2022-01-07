161a3b65f3d659bdc6af79cbea8369cbab65ff9c
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-07 12:48:19 -0900

    add details rout

--- Factory/Controllers/EngineerController.cs
+++ Factory/Controllers/EngineerController.cs
@@ -30,6 +30,13 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-    
+    public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+        .Include(Engineer => Engineer.JoinEntities)
+        .ThenInclude(join => join.Engineer)
+        .FirstOrDefault(Engineer => Engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
   }
 }
