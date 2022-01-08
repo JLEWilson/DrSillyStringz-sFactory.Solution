7218b511cbb16e5314362a6b2cef6933e41ad42c
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-07 16:41:09 -0900

    update splashpage to display all engineers and machines

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
-    public class HomeController : Controller
+
+  public class HomeController : Controller
+  {
+    public readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      var engineers =_db.Engineers.ToList();
+      var machines =_db.Machines.ToList();
+      ViewBag.Engineers = engineers;
+      ViewBag.Machines = machines;
+      return View();
+    }
+  }
 }
